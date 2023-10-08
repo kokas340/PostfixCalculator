d125a6cebb964dd328a16f6d79bdc4ed01796faa
Kristians Danilovs <k.danilovs2905@gmail.com>
2023-10-08 20:33:20 +0200

    tests

--- src/test/java/com/example/demo/PostfixCalculatorApplicationTests.java
+++ src/test/java/com/example/demo/PostfixCalculatorApplicationTests.java
@@ -1,13 +1,64 @@
 package com.example.demo;
 
+import com.example.demo.services.CalculatorServices;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class PostfixCalculatorApplicationTests {
+@AutoConfigureMockMvc
+public class PostfixCalculatorApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private CalculatorServices calculatorServices;
 
 	@Test
-	void contextLoads() {
+	public void testValidExpressionController() throws Exception {
+		// Mock the CalculatorServices result for a valid expression
+		when(calculatorServices.getResult(new ArrayList<>())).thenReturn(10.0);
+
+		// Create an example valid expression as JSON
+		String requestBody = "[{\"value\": 2.0, \"operator\": false}, " +
+				"{\"value\": 3.0, \"operator\": false}, " +
+				"{\"symbol\": \"+\", \"operator\": true}]";
+
+		ResultActions resultActions = mockMvc.perform(post("/calculator")
+				.contentType("application/json")
+				.content(requestBody));
+
+		resultActions.andExpect(status().isOk())
+				.andExpect(content().string("10.0"));
 	}
 
+	@Test
+	public void testInvalidExpressionController() throws Exception {
+		// Mock the CalculatorServices result for an invalid expression
+		when(calculatorServices.getResult(new ArrayList<>()))
+				.thenThrow(new ArithmeticException("Division by zero is not allowed"));
+
+		// Create an example invalid expression as JSON
+		String requestBody = "[{\"value\": 2.0, \"operator\": false}, " +
+				"{\"value\": 0.0, \"operator\": false}, " +
+				"{\"symbol\": \"/\", \"operator\": true}]";
+
+		ResultActions resultActions = mockMvc.perform(post("/calculator")
+				.contentType("application/json")
+				.content(requestBody));
+
+		resultActions.andExpect(status().isInternalServerError());
+	}
 }
--- src/test/java/com/example/demo/services/CalculatorServicesTest.java
+++ src/test/java/com/example/demo/services/CalculatorServicesTest.java
@@ -1,12 +1,44 @@
 package com.example.demo.services;
 
+import com.example.demo.model.TokenDTO;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
 
-class CalculatorServicesTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CalculatorServicesTest {
 
     @Test
-    void getResult() {
+    public void testValidExpression() {
+        CalculatorServices calculatorServices = new CalculatorServices();
+
+        // Create an example token list for a valid expression: "2 3 + 4 *"
+        ArrayList<TokenDTO> tokenList = new ArrayList<>();
+        tokenList.add(new TokenDTO(2.0, null, false));
+        tokenList.add(new TokenDTO(3.0, null, false));
+//        tokenList.add(new TokenDTO(null, '+', true));
+        tokenList.add(new TokenDTO(4.0, null, false));
+//        tokenList.add(new TokenDTO(null, '*', true));
+
+        double result = calculatorServices.getResult(tokenList);
+
+        // Assert that the result is as expected
+        assertEquals(20.0, result);
+    }
+
+    @Test
+    public void testInvalidExpression() {
+        CalculatorServices calculatorServices = new CalculatorServices();
+
+        // Create an example token list for an invalid expression: "2 0 /"
+        ArrayList<TokenDTO> tokenList = new ArrayList<>();
+        tokenList.add(new TokenDTO(2.0, null, false));
+        tokenList.add(new TokenDTO(0.0, null, false));
+//        tokenList.add(new TokenDTO(null, '/', true));
+
+        // Assert that an ArithmeticException is thrown when dividing by zero
+        assertThrows(ArithmeticException.class, () -> calculatorServices.getResult(tokenList));
     }
 }
